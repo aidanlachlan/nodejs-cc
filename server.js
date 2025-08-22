47cf0d14651f1de4503d32b2ada56b0c3433b31e
Aidan <aidanlmcmurray@gmail.com>
2025-08-21 17:18:20 -0700

    using postman

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-import http from 'http';
-const PORT = 8000;
+import http from "http";
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-
-    res.writeHead(500, {'content-type': 'application/json'});
-    res.end(JSON.stringify({ message: 'Internal Server Error' }));
-})
+  try {
+    // check if GET request
+    if (req.method === "GET") {
+      // handle GET request
+      if (req.url === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>About Page</h1>");
+      } else {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>404 Not Found</h1>");
+      }
+    } else {
+      throw new Error("method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "content-type": "text/plain" });
+    res.end("Server Error: " + error.message);
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
+  console.log(`Server is running on port ${PORT}`);
+});
