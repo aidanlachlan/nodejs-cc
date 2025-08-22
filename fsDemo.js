66082f766ed302c9cf813870ac7d3fe92d6c8700
Aidan <aidanlmcmurray@gmail.com>
2025-08-22 16:10:48 -0700

    writing and appending

--- fsDemo.js
+++ fsDemo.js
@@ -27,4 +27,29 @@ const readFile = async () => {
     }
 }
 
+// writeFile()
+
+const writeFile = async () => {
+    try {
+        await fs.writeFile('./test.txt', 'Hello, I am writing to this file')
+        console.log('File written to...')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// appendFile()
+const appendFile = async () => {
+    try {
+        await fs.appendFile('./test.txt', '\nThis is appended text')
+        console.log('File appended to...')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+writeFile()
+appendFile()
 readFile()
